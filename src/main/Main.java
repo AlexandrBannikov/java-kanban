fa82920e9f5ee3036fd897254bd123c8674551e1
Alexandr Bannikov <7932417721@mail.ru>
2023-10-22 17:26:44 +0300

    ТЗ 3 Second commit

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,3 +1,5 @@
+package main;
+
 import business.Epic;
 import business.Subtask;
 import business.Task;
@@ -11,19 +13,19 @@ public class Main {
         TaskManager manager = new TaskManager();
 
         // Создание
-        Task task1 = new Task("business.Task #1, финальное задание Яндекс практикум", "Task1 description", Status.NEW);
-        Task task2 = new Task("business.Task #2", "Task2 description", Status.NEW);
+        Task task1 = new Task("Task #1, финальное задание Яндекс практикум", "Task1 description", Status.NEW);
+        Task task2 = new Task("Task #2", "Task2 description", Status.NEW);
         int taskId1 = manager.addNewTask(task1);
         int taskId2 = manager.addNewTask(task2);
 
-        Epic epic1 = new Epic("business.Epic #1", "Epic1 description", Status.NEW);
-        Epic epic2 = new Epic("business.Epic #2", "Epic2 description",Status.NEW);
+        Epic epic1 = new Epic("Epic #1", "Epic1 description", Status.NEW);
+        Epic epic2 = new Epic("Epic #2", "Epic2 description",Status.NEW);
         int epicId1 = manager.addNewEpic(epic1);
         int epicId2 = manager.addNewEpic(epic2);
 
-        Subtask subtask1 = new Subtask("business.Subtask #1 - 1", "business.Subtask 1 description", Status.NEW, epicId1);
-        Subtask subtask2 = new Subtask("business.Subtask #2 - 1", "business.Subtask 2 description", Status.NEW, epicId1);
-        Subtask subtask3 = new Subtask("business.Subtask #3 - 2", "business.Subtask 3 description", Status.NEW, epicId2);
+        Subtask subtask1 = new Subtask("Subtask #1 - 1", "Subtask 1 description", Status.NEW, epicId1);
+        Subtask subtask2 = new Subtask("Subtask #2 - 1", "Subtask 2 description", Status.NEW, epicId1);
+        Subtask subtask3 = new Subtask("Subtask #3 - 2", "Subtask 3 description", Status.NEW, epicId2);
 
         Integer subtaskId1 = manager.addNewSubtask(subtask1);
         Integer subtaskId2 = manager.addNewSubtask(subtask2);
@@ -38,11 +40,11 @@ public class Main {
         }
 
         for (Epic epic : manager.getEpics()) {
-            System.out.println("business.Epic " + epic.getDescription());
+            System.out.println("Epic " + epic.getDescription());
         }
 
         for (Epic epic : manager.getEpics()) {
-            System.out.println("business.Epic " + epic.getId());
+            System.out.println("Epic " + epic.getId());
         }
 
         for (Subtask subtask : manager.getSubtasks()) {
